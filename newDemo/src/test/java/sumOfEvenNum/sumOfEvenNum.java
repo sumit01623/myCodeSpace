32124f9ff6370bc0ef5984ba5a66482aeae8f3a6
Rashmi Shakya <rashmishakya@Rashmis-MacBook-Pro.local>
2024-09-23 18:05:00 -0400

    sum of even numbers

---
+++ newDemo/src/test/java/sumOfEvenNum/sumOfEvenNum.java
@@ -0,0 +1,20 @@
+package sumOfEvenNum;
+
+public class sumOfEvenNum {
+	
+	public static void main(String[] args) {
+		
+		int sum = 0 ;
+		
+		for (int i=1; i<=10 ; i++); {
+			
+			if(sum%2==0) {
+				sum = sum+sum; {
+					System.out.println("sum of even number : "+sum);
+				}
+			}
+		}
+		
+	}
+
+}
