046d1c3f63a39e0322eb94013eb9c6ffc1db21da
rashmishakya <rashmishakya@localhost>
2024-09-22 20:42:45 -0400

    code to swap numbers of a and b.

---
+++ newDemo/src/test/java/swapNumbers/swapNumbers.java
@@ -0,0 +1,19 @@
+package swapNumbers;
+
+public class swapNumbers {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 100;
+		int c;
+		
+		c = a;
+		a = b;
+		b = c;
+		
+		System.out.println("value of a is: " +a);
+		System.out.println("value of b is: "+b);
+	}
+	
+
+}
+
