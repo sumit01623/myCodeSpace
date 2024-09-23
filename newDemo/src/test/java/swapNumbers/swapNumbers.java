1ba9b5a892aeb607e300d0bb7d6a5aa2567d5bd7
Rashmi Shakya <rashmishakya@Rashmis-MacBook-Pro.local>
2024-09-22 21:20:18 -0400

    added int c

--- newDemo/src/test/java/swapNumbers/swapNumbers.java
+++ newDemo/src/test/java/swapNumbers/swapNumbers.java
@@ -4,14 +4,17 @@ public class swapNumbers {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 100;
-		int c;
+		int c = 200;
+		int d;
 		
-		c = a;
+		d = a;
 		a = b;
 		b = c;
+		c = d;
 		
-		System.out.println("value of a is: " +a);
+		System.out.println("value of a is: "+a);
 		System.out.println("value of b is: "+b);
+		System.out.println("value of c is: "+c);
 	}
 	
 
